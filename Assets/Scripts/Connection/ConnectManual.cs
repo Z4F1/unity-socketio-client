e454b10bcc912f853fb577e7a940ee3f1802b384
Pelle Pastoor <pelle.pastoor217@gmail.com>
2017-08-15 23:49:05 +0200

    Start menu
    Fixed some ui glitches in start menu. Made a connect through ip and port.

---
+++ Assets/Scripts/Connection/ConnectManual.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ConnectManual : MonoBehaviour {
+
+    public GameObject serverBrowserUI;
+
+    public InputField ipInput;
+    public InputField portInput;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ConnectToServer();
+        }
+    }
+
+    public void UseIpScreen(bool active)
+    {
+        serverBrowserUI.SetActive(!active);
+    }
+
+    public void ConnectToServer()
+    {
+        string ip = ipInput.text;
+        string port = portInput.text;
+
+        PlayerPrefs.SetString("ip", ip);
+        PlayerPrefs.SetString("port", port);
+        SceneManager.LoadScene(1);
+    }
+
+}
